8e812fcac77bc723703062c52c37a8fc45195b6c
foxzin-0635 <jaoviniciussantiago988@gmail.com>
2026-06-26 19:41:29 -0300

    Update Common.lua

--- projects-required/luau-in-luau/src/Common/Common.lua
+++ projects-required/luau-in-luau/src/Common/Common.lua
@@ -1,5 +1,7 @@
--- luau-lang/luau/Common/include/Luau/Common.h
+-- Roblox Luau port of "luau-lang/luau/Common/include/Luau/Common.h"
 local Common = {}
+
+local Luau = {} -- namespace
 
 export type AssertHandler = {
   expr: string,
@@ -7,7 +9,15 @@ export type AssertHandler = {
   func: string
 }
 
-local Luau = {} -- namespace
+-- assertHandler() is not here since it relies on nullptr (empty pointer)
+
+local function assertCallHandler(expr: string, file: string, func: string): AssertHandler
+  return {
+    expr = expr,
+    file = file,
+    func = func
+  }
+end
 
 local FValue = (function()
   local module = {}
@@ -98,8 +108,16 @@ local FValueVersionSetter = (function()
 end)()
 
 -- FFlags
-local function DefineFlag<T>(params: {name: string, def: boolean, dynamic: boolean})
-  return FValue.new<params.def>(params.name, params.def, params.dynamic)
+local function DefineFastFlag<T>(name: string, def: T, dynamic: boolean)
+  return FValue.new(name, def, dynamic)
 end
+
+-- Combine all
+Luau.FValue = FValue
+Luau.FValueVersionSetter = FValueVersionSetter
+Luau.DefineFastFlag = DefineFastFlag
+
+Common.Namespaces = {}
+Common.Namespaces.Luau = Luau
 
 return Common
