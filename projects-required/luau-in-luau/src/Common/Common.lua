87266d0d4c7f9bc4f49873caee2e7b00bec3c9b2
foxzin-0635 <jaoviniciussantiago988@gmail.com>
2026-06-26 01:56:21 -0300

    Add Common.lua from luau-in-luau

--- projects-required/luau-in-luau/src/Common/Common.lua
+++ projects-required/luau-in-luau/src/Common/Common.lua
@@ -1,13 +1,56 @@
+-- luau-lang/luau/Common/include/Luau/Common.h
 local Common = {}
 
-type AssertHandler = {
+export type AssertHandler = {
   expr: string,
   file: string,
   func: string
 }
 
-local Luau = {}
+local Luau = {} -- namespace
+
 local FValue = (function()
+  local module = {}
+  local metatable = {
+    __metatable = "The metatable is locked",
+    __index = module,
+    __tostring = function(t)
+      return "FValue"
+    end
+  }
+  
+  type FValueType<T> = {
+    name: string,
+    dynamic: boolean,
+    value: T,
+    next: FValueType<any>?
+  }
+  
+  module.list = {
+    boolean = nil,
+    number = nil
+  }
+  module.version = 0
+  
+  function module.new<T>(name: string, def: T, dynamic: boolean)
+    local self = setmetatable({}, nil)
+    
+    self.value = def
+    self.dynamic = dynamic
+    self.name = name
+    local head = module.list[typeof(def)]
+    self.next = head
+    module.list[typeof(def)] = self
+    
+    setmetatable(self, metatable)
+    return self
+  end
+  
+  setmetatable(module, metatable)
+  return module
+end)()
+
+local FValueVersionSetter = (function()
   local module = {}
   local metatable = {
     __metatable = "The metatable is locked",
@@ -22,23 +65,41 @@ local FValue = (function()
     mtc.__newindex = function() return nil end
     return mtc
   end
-  FValue.list = {}
   
-  function module.new<T>(name: string, def: T, dynamic: boolean)
+  function module.new(name: string, version: number)
     local self = setmetatable({}, nil)
     
-    self.value = def
-    self.dynamic = dynamic
-    self.name = name
-    self.list = self
-    self.next = list
+    local found = false
+    
+    local fbool = FValue.list.boolean
+    while fbool do
+      if fbool.name == name then
+        fbool.version = version
+        found = true
+      end
+      fbool = fbool.next
+    end
+    
+    local fint = FValue.list.number
+    while fint do
+      if fint.name == name then
+        fint.version = version
+        found = true
+      end
+      fint = fint.next
+    end
     
     setmetatable(self, createInstanceMetatable())
     return self
   end
   
-  
+  setmetatable(module, metatable)
   return module
 end)()
+
+-- FFlags
+local function DefineFlag<T>(params: {name: string, def: boolean, dynamic: boolean})
+  return FValue.new<params.def>(params.name, params.def, params.dynamic)
+end
 
 return Common
